5480c01f9bba5e8c64e5973973d547db30111a5a
Artur de Maria <arturdemaria7@gmail.com>
2026-06-25 14:16:02 -0300

    feat/sahur: added sahur new setup

--- scripts/scene.js
+++ scripts/scene.js
@@ -2,12 +2,14 @@ var scene = {};
 
 async function setupScene() {
   const mapParts = await loadOBJ("./../assets/models/backrooms.obj");
-  const sahur = await loadOBJ("./../assets/models/sahur.obj");
-  
+  const sahurParts = await loadOBJ("./../assets/models/sahur.obj");
+
   scene.parts = [];
-  const carpetTex = loadTexture(gl, "assets/textures/carpet.jpeg", 0);
-  const wallTex = loadTexture(gl, "assets/textures/wallpaper.jpeg", 1);
-  const sahurTex = loadTexture(gl, "./../assets/textures/sahur.png", 2)
+  scene.monsterParts = [];
+
+  loadTexture(gl, "assets/textures/carpet.jpeg", 0);
+  loadTexture(gl, "assets/textures/wallpaper.jpeg", 1);
+  loadTexture(gl, "./../assets/textures/sahur.png", 2);
 
   for (const part of mapParts) {
     var buf = gl.createBuffer();
@@ -15,34 +17,25 @@ async function setupScene() {
     gl.bufferData(gl.ARRAY_BUFFER, part.bufferData, gl.STATIC_DRAW);
 
     let unit = 0;
-    if (part.materialName === "Carpet") {
-      unit = 0;
-    } else if (part.materialName === "Wall") {
-      unit = 1;
-    }
+    if (part.materialName === "Carpet") unit = 0;
+    else if (part.materialName === "Wall") unit = 1;
 
     scene.parts.push({
       buffer: buf,
       vertexCount: part.vertexCount,
       textureUnit: unit,
-      transform: mat4Identity(),
     });
   }
 
-  const sahurMatrix = math.multiply(
-    mat4Translate(1, 0, -1),
-    math.multiply(mat4Scale(0.75, 0.75, 0.75), mat4Identity()),
-  );
-
-  for (const obj of sahur) {
+  for (const obj of sahurParts) {
     var buf = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buf);
     gl.bufferData(gl.ARRAY_BUFFER, obj.bufferData, gl.STATIC_DRAW);
-    scene.parts.push({
+
+    scene.monsterParts.push({
       buffer: buf,
       vertexCount: obj.vertexCount,
       textureUnit: 2,
-      transform: sahurMatrix,
     });
   }
 }
@@ -52,7 +45,21 @@ function drawScene(projMat, viewMat) {
     drawObject(
       part.buffer,
       part.vertexCount,
-      part.transform,
+      mat4Identity(),
+      projMat,
+      viewMat,
+      part.textureUnit,
+      false,
+      null,
+    );
+  }
+
+  const monsterTransform = monster.getTransform();
+  for (const part of scene.monsterParts) {
+    drawObject(
+      part.buffer,
+      part.vertexCount,
+      monsterTransform,
       projMat,
       viewMat,
       part.textureUnit,
